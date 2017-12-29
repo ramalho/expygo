282be46f272c066ff48d137342b7a42f21392132
Luciano Ramalho <luciano@ramalho.org>
2017-12-29 10:48:32 -0200

    changed Python version to use PBKDF2 instead of just SHA-512

--- passdrill.py
+++ passdrill.py
@@ -7,9 +7,9 @@ import sys
 import hashlib
 import base64
 import getpass
+import os
 
-HASH_ALGORITHM = 'sha512'
-HASH_FILENAME = 'passdrill.' + HASH_ALGORITHM
+HASH_FILENAME = 'passdrill.hash'
 HELP = 'Use -s to save passphrase hash for practice.'
 
 
@@ -26,29 +26,51 @@ def prompt():
     return passwd
 
 
-def hash_bytes(text):
-    octets = hashlib.sha512(text.encode('utf-8')).digest()
-    return base64.b64encode(octets)
+def pbkdf2(salt, octets):
+    algorithm = 'sha512'
+    rounds = 100_000
+    return hashlib.pbkdf2_hmac(algorithm, octets, salt, rounds)
+
+
+def compute_hash(key_func, salt, text):
+    octets = text.encode('utf-8')
+    if key_func == 'pbkdf2':
+        return pbkdf2(salt, octets)
+    else:
+        raise ValueError('Unknown key function ' + repr(key_func))
+
+
+def build_hash(key_func, text):
+    salt = os.urandom(32)
+    octets = compute_hash(key_func, salt, text)
+    header = key_func.encode('utf-8') + b':' + base64.b64encode(salt)
+    return header + b':' + base64.b64encode(octets)
 
 
 def save_hash(argv):
     if len(argv) > 2 or argv[1] != '-s':
         print('ERROR: invalid argument.', HELP)
         sys.exit(1)
-    passwd_hash = hash_bytes(prompt())
+    wrapped_hash = build_hash('pbkdf2', prompt())
     with open(HASH_FILENAME, 'wb') as fp:
-        fp.write(passwd_hash)
-    print(f'Passphrase {HASH_ALGORITHM} hash saved to',
-          HASH_FILENAME)
+        fp.write(wrapped_hash)
+    print(f'Passphrase hash saved to', HASH_FILENAME)
+
+
+def unwrap_hash(wrapped_hash):
+    key_func, salt, passwd_hash = wrapped_hash.split(b':')
+    return (key_func.decode('utf-8'), base64.b64decode(salt),
+        base64.b64decode(passwd_hash))
 
 
 def practice():
     try:
         with open(HASH_FILENAME, 'rb') as fp:
-            passwd_hash = fp.read()
+            wrapped_hash = fp.read()
     except FileNotFoundError:
         print('ERROR: passphrase hash file not found.', HELP)
         sys.exit(2)
+    key_func, salt, passwd_hash = unwrap_hash(wrapped_hash)
     print('Type q to end practice.')
     response = ''
     turn = 0
@@ -63,7 +85,7 @@ def practice():
         elif response == 'q':
             turn -= 1  # don't count this response
             break
-        if hash_bytes(response) == passwd_hash:
+        if compute_hash(key_func, salt, response) == passwd_hash:
             correct += 1
             answer = 'OK'
         else:
